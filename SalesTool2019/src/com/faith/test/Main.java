d9c9c3b17a2276ce50906f7818d6d7af167985c2
USER <USER@smriz>
2019-09-23 15:14:28 +0530

    initial commit

---
+++ SalesTool2019/src/com/faith/test/Main.java
@@ -0,0 +1,17 @@
+package com.faith.test;
+
+public class Main {
+	
+	public static void main(String[] args){
+		SalesData objSalesData=new SalesData();
+		
+		System.out.println("Hello UST !!!");
+		displayGreetings();
+		objSalesData.display();
+	}
+	
+	private static void displayGreetings(){
+		System.out.println("Welcome");
+	}
+
+}
